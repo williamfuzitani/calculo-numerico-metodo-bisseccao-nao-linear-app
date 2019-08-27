0dbe9785bf3552f4a8d6c7d65ef1396e756ed920
William <william@imac-de-william.senailondrina.com>
2019-08-27 09:51:58 -0300

    fetching data

--- src/App.vue
+++ src/App.vue
@@ -1,116 +1,92 @@
 <template>
-  <div id="app" class="min-h-screen flex flex-col justify-center items-center bg-gray-600">
-    <div class="border rounded">
-      <form @submit.prevent="onSubmit" class="w-full p-2">
-        <div class="flex mb-2">
-          <div class="flex items-center">
-            <input v-model="inputX5" class="w-16 px-1 text-sm" type="number">
-            <label class="mx-2">x^5 +</label>
+  <div id="app" class="min-h-screen bg-gray-600 font-mono">
+    <div class="container mx-auto pt-12">
+      <div class="flex justify-center">
+        <form @submit.prevent="onSubmit" class="p-2 w-full">
+          <div class="flex mb-2">
+            <div class="flex items-center">
+              <input v-model="inputX5" class="w-20 px-1 text-sm rounded" type="text">
+              <label class="mx-1">x⁵ +</label>
+            </div>
+            <div class="flex items-center">
+              <input v-model="inputX4" class="w-20 px-1 text-sm rounded" type="text">
+              <label class="mx-1">x⁴ +</label>
+            </div>
+            <div class="flex items-center">
+              <input v-model="inputX3" class="w-20 px-1 text-sm rounded" type="text">
+              <label class="mx-1">x³ +</label>
+            </div>
+            <div class="flex items-center">
+              <input v-model="inputX2" class="w-20 px-1 text-sm rounded" type="text">
+              <label class="mx-1">x² +</label>
+            </div>
+            <div class="flex items-center">
+              <input v-model="inputX1" class="w-20 px-1 text-sm rounded" type="text">
+              <label class="mx-1">x +</label>
+            </div>
+            <div class="flex items-center">
+              <input v-model="inputX" class="w-20 px-1 text-sm rounded" type="text">
+              <label class="mx-1"></label>
+            </div>
           </div>
-          <div class="flex items-center">
-            <input v-model="inputX4" class="w-16 px-1 text-sm" type="number">
-            <label class="mx-2">x^4 +</label>
+          <div class="flex items-center my-6">
+            <label class="mr-2">Epsilon</label>
+            <input v-model="epsilon" class="w-20 px-1 text-sm rounded" type="text">
           </div>
-          <div class="flex items-center">
-            <input v-model="inputX3" class="w-16 px-1 text-sm" type="number">
-            <label class="mx-2">x^3 +</label>
-          </div>
-          <div class="flex items-center">
-            <input v-model="inputX2" class="w-16 px-1 text-sm" type="number">
-            <label class="mx-2">x^2 +</label>
-          </div>
-          <div class="flex items-center">
-            <input v-model="inputX1" class="w-16 px-1 text-sm" type="number">
-            <label class="mx-2">x +</label>
-          </div>
-          <div class="flex items-center">
-            <input v-model="inputX" class="w-16 px-1 text-sm" type="number">
-            <label class="mx-2"></label>
-          </div>
-        </div>
-        <div class="flex items-center my-6">
-          <label class="">Epsilon</label>
-          <input v-model="epsilon" class="w-16 px-1 text-sm" type="number">
-        </div>
 
-        <button class="border rounded py-1 px-2" type="submit">Enviar</button>
-      </form>
-    </div>
+          <button class="border rounded py-1 px-2" type="submit">Enviar</button>
+        </form>
+      </div>
 
-    <div class="flex items-center justify-around mt-2">
-      <div class="border rounded p-2">Intervalos</div>
-      <div class="border rounded p-2">Raizes</div>
-      <div class="border rounded p-2">Tabela</div>
+      <div class="flex items-center justify-around mt-4 border rounded p-2">
+        <div class="border rounded p-2">Intervalos</div>
+        <div class="border rounded p-2">Raizes</div>
+        <div class="border rounded p-2">Tabela</div>
+      </div>
     </div>
 
   </div>
 </template>
 
 <script>
-  import axios from 'axios'
   /* eslint-disable */
   export default {
     name: 'app',
     data() {
       return {
-        inputX5: {
-          type: Number,
-          required: true
-        },
-        inputX4: {
-          type: Number,
-          required: true
-        },
-        inputX3: {
-          type: Number,
-          required: true
-        },
-        inputX2: {
-          type: Number,
-          required: true
-        },
-        inputX1: {
-          type: Number,
-          required: true
-        },
-        inputX: {
-          type: Number,
-          required: true
-        },
-        epsilon: {
-          type: Number,
-          required: true
-        }
+        inputX5: null,
+        inputX4: null,
+        inputX3: null,
+        inputX2: null,
+        inputX1: null,
+        inputX: null,
+        epsilon: null
       }
     },
 
     methods: {
       async onSubmit() {
-        console.log('submit')
         const BASE_URL = 'https://calculo-numerico.herokuapp.com/api/calculo-numerico'
-        let reqData = {
-          inputX5: this.inputX5,
-          inputX4: this.inputX4,
-          inputX3: this.inputX3,
-          inputX2: this.inputX2,
-          inputX1: this.inputX1,
-          inputX: this.inputX,
-          inputEpsilon: this.epsilon
+        const reqBody = {
+          "entradaSeis": this.inputX5,
+          "entradaCinco": this.inputX4,
+          "entradaQuatro": this.inputX3,
+          "entradaTres": this.inputX2,
+          "entradaDois": this.inputX1,
+          "entradaUm": this.inputX,
+          "entradaEpsilon": this.epsilon
+        }
+        const options = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(reqBody)
         }
 
-        await axios({
-          method: 'post',
-          url: BASE_URL,
-          headers: {
-            "Content-Type": "application/json",
-            'Access-Control-Allow-Origin' : '*'
-          },
-          body: JSON.stringify(reqData)
-        }).then(function (response) {
-          console.log(response)
-        }).catch(function (error) {
-          console.log(error)
-        })
+        const response = await fetch(BASE_URL, options)
+        const data = await response.json()
+        console.log(data)
       }
 
     }
