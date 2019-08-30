a2add49b04964c312e7ed3b5f82e0e9762662e12
William <william@imac-de-william.senailondrina.com>
2019-08-30 10:43:17 -0300

    tabelas

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,78 @@
 <template>
-  <div id="app" class="min-h-screen bg-gray-600 font-mono">
-    <div class="container mx-auto pt-12">
-      <div class="flex justify-center">
-        <form @submit.prevent="onSubmit" class="p-2 w-full">
+  <div id="app" class="bg-gray-600 font-mono">
+    <div class="container mx-auto min-h-screen">
+      <div class="flex justify-center items-center">
+        <form @submit.prevent="onSubmit" class="p-2">
           <div class="flex mb-2">
             <div class="flex items-center">
-              <input v-model="inputX5" class="w-20 px-1 text-sm rounded" type="text">
+              <input v-model="inputX5" class="w-16 h-8 px-1 text-sm rounded" type="text">
               <label class="mx-1">x⁵ +</label>
             </div>
             <div class="flex items-center">
-              <input v-model="inputX4" class="w-20 px-1 text-sm rounded" type="text">
+              <input v-model="inputX4" class="w-16 h-8 px-1 text-sm rounded" type="text">
               <label class="mx-1">x⁴ +</label>
             </div>
             <div class="flex items-center">
-              <input v-model="inputX3" class="w-20 px-1 text-sm rounded" type="text">
+              <input v-model="inputX3" class="w-16 h-8 px-1 text-sm rounded" type="text">
               <label class="mx-1">x³ +</label>
             </div>
             <div class="flex items-center">
-              <input v-model="inputX2" class="w-20 px-1 text-sm rounded" type="text">
+              <input v-model="inputX2" class="w-16 h-8 px-1 text-sm rounded" type="text">
               <label class="mx-1">x² +</label>
             </div>
             <div class="flex items-center">
-              <input v-model="inputX1" class="w-20 px-1 text-sm rounded" type="text">
+              <input v-model="inputX1" class="w-16 h-8 px-1 text-sm rounded" type="text">
               <label class="mx-1">x +</label>
             </div>
             <div class="flex items-center">
-              <input v-model="inputX" class="w-20 px-1 text-sm rounded" type="text">
+              <input v-model="inputX" class="w-16 h-8 px-1 text-sm rounded" type="text">
               <label class="mx-1"></label>
             </div>
           </div>
           <div class="flex items-center my-6">
             <label class="mr-2">Epsilon</label>
-            <input v-model="epsilon" class="w-20 px-1 text-sm rounded" type="text">
+            <input v-model="epsilon" class="w-16 h-8 px-1 text-sm rounded" type="text">
           </div>
 
           <button class="border rounded py-1 px-2" type="submit">Enviar</button>
         </form>
       </div>
 
-      <div class="flex items-center justify-around mt-4 border rounded p-2">
-        <div class="border rounded p-2">Intervalos</div>
-        <div class="border rounded p-2">Raizes</div>
-        <div class="border rounded p-2">Tabela</div>
+      <div class="flex items-center mt-4">
+        <table class="w-full text-left text-sm shadow-md">
+          <thead class="bg-gray-800 text-white w-full flex rounded-t">
+            <tr class="border-b flex justify-center w-full">
+              <th class="p-2 w-1/4">Intervalo</th>
+              <th class="p-2 w-1/4">Resultado da Funcao</th>
+            </tr>
+          </thead>
+          <tbody class="flex flex-col items-center w-full">
+            <tr v-for="(item, index) in data" :key="index" class="flex justify-center w-full border-b">
+              <td class="p-2 w-1/4">[ {{ item.intervalo.a }}, {{ item.intervalo.a }} ]</td>
+              <td class="p-2 w-1/4">{{ item.raizDaFuncao }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div class="flex items-center justify-center mt-4">
+        <table class="w-full text-left text-sm shadow-md">
+          <thead class="bg-gray-800 text-white w-full flex rounded-t">
+            <tr class="border-b flex justify-center w-full">
+              <th class="p-2 w-1/4">a</th>
+              <th class="p-2 w-1/4">b</th>
+            </tr>
+          </thead>
+          <tbody class="flex flex-col items-center w-full">
+            <tr v-for="(item, index) in data" :key="index" class="flex justify-center w-full border-b">
+              <td class="p-2 w-1/4">{{ item.intervalosIniciais[0].a }}</td>
+              <td class="p-2 w-1/4">{{ item.intervalosIniciais[0].b }}</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
+
 
   </div>
 </template>
@@ -60,7 +89,8 @@
         inputX2: null,
         inputX1: null,
         inputX: null,
-        epsilon: null
+        epsilon: null,
+        data: null
       }
     },
 
@@ -86,6 +116,7 @@
 
         const response = await fetch(BASE_URL, options)
         const data = await response.json()
+        this.data = data
         console.log(data)
       }
 
